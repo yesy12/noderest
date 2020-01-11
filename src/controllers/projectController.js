69249d2e311641fe9e9bf41448ec838459dd98b0
Alison <alisonvieira.av29@gmail.com>
2020-01-11 18:28:29 -0300

    projectControllers]

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
+const authMiddleware = require("../middlewares/auth");
+
 const router = express.Router();
+
+router.use(authMiddleware);
 
 router.get("/",(req,res)=>{
     res.send({
