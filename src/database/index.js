92f48b5ee3fb3c4e081e2a3495426e7a5c065f83
Alison <alisonvieira.av29@gmail.com>
2020-01-11 16:50:06 -0300

    database config

---
+++ src/database/index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const username = "noderest";
+const password = "Qbg4IKFcKCFQmSpW";
+const db = "noderest";
+
+const url = `mongodb+srv://${username}:${password}@cluster0-nth3w.mongodb.net/${db}?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
