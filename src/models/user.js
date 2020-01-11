05932f8d8c708f3880eb29be0608f76bc3602bba
Alison <alisonvieira.av29@gmail.com>
2020-01-11 17:20:23 -0300

    alterado model user

--- src/models/user.js
+++ src/models/user.js
@@ -13,7 +13,7 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true,
+        require: true,
         select : false
     },
     createdAt: {
