d538ee2b97754eb6d086e002d198f636d8a26ca0
Alison <alisonvieira.av29@gmail.com>
2020-01-23 20:05:24 -0300

    rotas

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,16 +1,45 @@
 const express = require("express");
 
 const authMiddleware = require("../middlewares/auth");
+const Project = require("../models/projects");
+const Task = require("../models/task");
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get("/",(req,res)=>{
+router.get("/", async (req,res)=>{
     res.send({
-        ok : true,
         user : req.useId
     })
 })
+
+router.post("/", async (req,res)=>{
+    res.send({
+        user : req.useId
+    })
+})
+
+router.get("/:projectId",async (req,res)=>{
+    res.send({
+        user : req.useId
+    })
+})
+
+
+router.put("/:projectId",async (req,res)=>{
+    res.send({
+        user : req.useId
+    })
+})
+
+router.delete("/:projectId",async (req,res)=>{
+    res.send({
+        user : req.useId
+    })
+})
+
+
+
 
 module.exports = router;
